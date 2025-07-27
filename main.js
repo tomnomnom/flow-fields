5147eaa8cf9088496d155a1047e87cecc241573d
Tom Hudson <mail@tomnomnom.com>
2025-07-28 00:05:22 +0100

    Green square waves

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ const addVec = ([x1, y1], [x2, y2]) => [x1+x2, y1+y2];
 const scaleVec = ([x, y], sc) => [x*sc, y*sc];
 const randAngle = () => Math.random() * 2 * Math.PI;
 
-const particleCount = 4000;
-const particleRadius = 1;
+const particleCount = 8000;
+const particleRadius = 2;
 const randParticle = () => [Math.random()*w, Math.random()*h];
 const particles = Array.from({length: particleCount}, randParticle);
 
@@ -24,13 +24,13 @@ const randLifetime = () => Math.floor(Math.random() * maxLifetime);
 const lifetimes = Array.from({length: particleCount}, randLifetime);
 
 const calculateVector = ([x, y], t, l) => {
-    const comps = [
-        Math.sin((x-y)*0.02)*(t/6000),
-        Math.sin((x+y)*0.01)*(t/6000),
-    ];
-    const avg = comps.reduce((x, y) => x + y, 0) / comps.length;
+    x = x/w*50;
+    y = y/h*20;
 
-    return radToVec(avg * Math.PI);
+    const n = Math.sin(x)+Math.sin(x*3)/3+Math.sin(x*5)/5 +
+              Math.sin(y)+Math.sin(y*3)/3+Math.sin(y*5)/5;
+
+    return radToVec(n * Math.PI * t/100000);
 };
 
 ctx.fillStyle = 'hsl(0 0 0)';
@@ -52,10 +52,10 @@ const draw = (t) => {
 
         const fv = calculateVector([x, y], t, lifetimes[i]);
         particles[i] = addVec(particles[i], fv);
+        particles[i] = addVec(particles[i], fv);
 
-        const hue = Math.floor(lifetimes[i]/maxLifetime*250)
-        ctx.fillStyle = 'hsl('+hue+' 100 255 / 5%)';
-        ctx.fillStyle = 'hsl(0 0 255 / 5%)';
+        const hue = Math.floor(lifetimes[i]/maxLifetime*100)+250
+        ctx.fillStyle = 'hsl('+hue+' 60 255 / 5%)';
 
         ctx.beginPath();
         ctx.arc(x, y, particleRadius, 0, Math.PI*2);
